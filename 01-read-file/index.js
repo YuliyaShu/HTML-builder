ea553db4dda540b49738de4cc40fea88178f9d7a
YuliyaShu <juliette7373@gmail.com>
2022-05-20 09:10:41 +0200

    feat: 01-read-file solve

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+const pathToText = path.join(__dirname,'/text.txt');
+const stream = fs.createReadStream(pathToText, 'utf-8');
+stream.on('data', chunk => console.log(chunk));
+stream.on('error', error => console.log('Error', error.message));
