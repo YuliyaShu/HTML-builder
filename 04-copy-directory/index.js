62dc988e9326ccab7d6419b614e2c16dc366bdef
YuliyaShu <juliette7373@gmail.com>
2022-05-21 15:28:53 +0200

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.join(__dirname,'/files-copy');
+
+const obj ={
+  removeDir() {
+    fs.rm(dir, { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      obj.createDir();
+      obj.copyFiles();
+    });
+  },
+
+  createDir() {
+    fs.mkdir(dir, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('Folder created!');
+    });
+  },
+
+  copyFiles() {
+    const pathFiles = path.join(__dirname, './files');
+    fs.readdir(pathFiles, {withFileTypes: true}, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          if (!file.isDirectory()) {
+            fs.copyFile(path.join(pathFiles, file.name), path.join(dir, file.name), (err) => {
+              if (err) {
+                console.log('Error Found:', err);
+              } 
+            });
+          }
+        });
+      }
+    });
+  },
+
+
+};
+
+
+fs.exists(dir, (exist) => {
+  if (exist)    {
+    obj.removeDir();
+  } else {
+    obj.createDir();
+    obj.copyFiles();
+  }
+});
 
